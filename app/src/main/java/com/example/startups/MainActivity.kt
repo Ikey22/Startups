b1d794d8bb1662911b4bb673df051d8ea8a77eba
Ikey22 <ikechukwuachom@gmail.com>
2020-03-23 16:28:16 +0000

    Nav_view commit

--- app/src/main/java/com/example/startups/MainActivity.kt
+++ app/src/main/java/com/example/startups/MainActivity.kt
@@ -2,31 +2,126 @@ package com.example.startups
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import java.nio.channels.AsynchronousFileChannel.open
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var logoutFragment: LogoutFragment
+    lateinit var messageFragment: MessageFragment
+    lateinit var profileFragment: ProfileFragment
+    lateinit var settingFragment: SettingFragment
+    lateinit var timelineFragment: TimelineFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val message : EditText = findViewById(R.id.writeText)
-        val submitText : TextView = findViewById(R.id.btnSubmit)
+        setSupportActionBar(toolBar)
+        val actionBar = supportActionBar
+        actionBar?.title = "Navigation Drawer"
+
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this, drawerLayout, toolBar,(R.string.open), (R.string.close)
+        ){
 
 
-        submitText.setOnClickListener {
+        }
 
-            var messageGotten = message.text.toString()
-            val intent = Intent(this, Myactivity ::class.java)
-            intent.putExtra("Message is received: ", messageGotten)
-            startActivity(intent)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
+        nav_view.setNavigationItemSelectedListener(this)
 
+        //we now implement navigation item selected listener
+        //the default fragment is home fragment
 
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+    }
+
+    override fun onNavigationItemSelected(menuitem: MenuItem): Boolean {
+    //create related fragments
+        when(menuitem.itemId)
+        {
+            R.id.home ->{
+                homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,homeFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.logout ->{
+                logoutFragment = LogoutFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,logoutFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.message ->{
+                messageFragment = MessageFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,messageFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.profile ->{
+                profileFragment = ProfileFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,profileFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.setting ->{
+                settingFragment = SettingFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,settingFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+            R.id.timeline ->{
+                timelineFragment = TimelineFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_layout,timelineFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+
+    }
+
+    override fun onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            super.onBackPressed()
         }
     }
 }
